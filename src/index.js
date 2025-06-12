02da613f96099da65741884ac38a1d6bd4e2b399
Jordyfr2004 <jordyalex32@gmail.com>
2025-06-11 23:36:22 -0500

    neww4

--- src/index.js
+++ src/index.js
@@ -5,20 +5,31 @@ import cors from 'cors';
 
 const app = express()
 
+// Desactiva el header "X-Powered-By"
+app.disable('x-powered-by');
+
 app.use(express.json())
 
 // CORS configurado de forma segura
 const corsOptions = {
-  origin: 'https://trustedwebsite.com' // Compliant
+  origin: 'https://trustedwebsite.com'
 };
 
 app.use(cors(corsOptions));
-
 app.use(rutas)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+
+
+
+
+
+
+
+
+
 
 
 

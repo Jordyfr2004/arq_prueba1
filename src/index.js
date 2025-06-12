ad2336713f11005698d06878e8a807b173ce952b
Jordyfr2004 <jordyalex32@gmail.com>
2025-06-11 23:21:21 -0500

    cambios new2

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@ import rutas from './routes/users.routes.js'
 import { port } from './config.js'
 import cors from 'cors';
 
-
-
 const app = express()
 
 app.use(express.json())
-app.use(cors());
+
+// CORS configurado de forma segura
+const corsOptions = {
+  origin: 'https://trustedwebsite.com' // Compliant
+};
+
+app.use(cors(corsOptions));
 
 app.use(rutas)
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+
+
+
+
+
+
+
 
 
 //usar npm run dev para ejecutar en el terminal

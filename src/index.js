e954acdb874e24cdda96b11f4721f258ed7f8ceb
Jordyfr2004 <jordyalex32@gmail.com>
2025-06-11 23:25:30 -0500

    new 3

--- src/index.js
+++ src/index.js
@@ -3,23 +3,19 @@ import rutas from './routes/users.routes.js'
 import { port } from './config.js'
 import cors from 'cors';
 
+
+
 const app = express()
 
 app.use(express.json())
-
-// CORS configurado de forma segura
-const corsOptions = {
-  origin: 'https://trustedwebsite.com' // Compliant
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(rutas)
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
-
 
 
 
